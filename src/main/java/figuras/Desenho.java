fbe587d91445a5ba7b59aec141988ef55d4b0fa0
Franco <francobmaranhao@gmail.com>
2025-04-15 10:51:28 -0300

    Update Desenho.java

--- src/main/java/figuras/Desenho.java
+++ src/main/java/figuras/Desenho.java
@@ -3,25 +3,22 @@ package figuras;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Desenho {
+public class Desenho 
+{
 
     private List<Figura> figuras = null;
 
-    public Desenho() {
-        figuras = new ArrayList<>();
-    }
+    public Desenho() figuras = new ArrayList<>();
 
-    public List<Figura> getFiguras() {
-        return figuras;
-    }
+    public List<Figura> getFiguras() return figuras;
 
-    public void adicionar(Figura figura) {
-        figuras.add(figura);
-    }
+    public void adicionar(Figura figura) figuras.add(figura);
 
-    public double getArea() {
+    public double getArea() 
+    {
         double total = 0;
-        for (int i = 0; i < figuras.size(); i++) {
+        for (int i = 0; i < figuras.size(); i++) 
+        {
             total = total + figuras.get(i).getArea();
         }
         return total;
