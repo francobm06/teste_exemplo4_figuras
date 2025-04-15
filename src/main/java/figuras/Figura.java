bf80fc04a0dfa0e4b16cc40138cb8b78a6279d96
Franco <francobmaranhao@gmail.com>
2025-04-15 11:14:24 -0300

    Update Figura.java

--- src/main/java/figuras/Figura.java
+++ src/main/java/figuras/Figura.java
@@ -1,10 +1,21 @@
 package figuras;
 
-public abstract class Figura 
-{
+public abstract class Figura {
+
     private String cor;
-    protected Figura(String cor) this.cor = cor;
-    public String getCor() return cor;
-    public void setCor(String cor) this.cor = cor;
-    public abstract double getArea(); 
+
+    protected Figura(String cor) {
+        this.cor = cor;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public abstract double getArea();
+
 }
