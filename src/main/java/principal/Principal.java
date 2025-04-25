d08283f08ef2014c39945659f0ba0fe35a9f0610
Franco <francobmaranhao@gmail.com>
2025-04-25 16:33:29 -0300

    Update Principal.java

--- src/main/java/principal/Principal.java
+++ src/main/java/principal/Principal.java
@@ -5,21 +5,17 @@ import figuras.Retangulo;
 import figuras.Triangulo;
 import javax.swing.JOptionPane;
 
-public class Principal {
+public class Principal{
 
     public static void main(String[] args){
         String opcao = "";
         Desenho desenho = new Desenho();
          
-        while (!opcao.equals("9")) {
-            opcao = JOptionPane.showInputDialog("1 - Adiciona um Triângulo "
-                    + "\n2 - Adiciona um Retângulo "
-                    + "\n3 - Listar figuras do desenho"
-                    + "\n4 - Área desenho"
-                    + "\n9 - Sair");
+        while (!opcao.equals("9")){
+            opcao = JOptionPane.showInputDialog("1 - Adiciona um Triângulo " + "\n2 - Adiciona um Retângulo " + "\n3 - Listar figuras do desenho" + "\n4 - Área desenho" + "\n9 - Sair");
 
-            switch (opcao) {
-                case "1": {
+            switch (opcao){
+                case "1":{
                     Triangulo triangulo = new Triangulo();
                     triangulo.setCor(JOptionPane.showInputDialog("Digite a Cor do Triângulo"));
                     triangulo.setBase(Double.parseDouble(JOptionPane.showInputDialog("Digite a Base do Triângulo")));
@@ -29,7 +25,7 @@ public class Principal {
                     desenho.adicionar(triangulo);
                     break;
                 }            
-                case "2": {
+                case "2":{
                     Retangulo retangulo = new Retangulo();
                     retangulo.setCor(JOptionPane.showInputDialog("Digite a Cor do Retângulo"));
                     retangulo.setBase(Double.parseDouble(JOptionPane.showInputDialog("Digite a Base do Retângulo")));
@@ -39,17 +35,17 @@ public class Principal {
                     desenho.adicionar(retangulo);
                     break;
                 } 
-                case "3": {
+                case "3":{
                     System.out.println("Lista das figuras do desenho");
                     for (int i=0;i<desenho.getFiguras().size();i++) System.out.println(i + "-" + desenho.getFiguras().get(i).getClass().getName() + " com area:" + desenho.getFiguras().get(i).getArea());
                     break;
                 }                
-                case "4": {
+                case "4":{
                     System.out.println("Area total do desenho:" + desenho.getArea());
                     break;
                 }
                   
-                case "9": {
+                case "9":{
                     JOptionPane.showMessageDialog(null, "Saindo do sistema.");
                     break;
                 }
