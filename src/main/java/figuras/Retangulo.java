2e863c49d9cb89964a576e4d4661c91eb8cb37a0
Franco <francobmaranhao@gmail.com>
2025-04-25 16:32:05 -0300

    Update Retangulo.java

--- src/main/java/figuras/Retangulo.java
+++ src/main/java/figuras/Retangulo.java
@@ -1,38 +1,38 @@
 package figuras;
 
-public class Retangulo extends Figura {
+public class Retangulo extends Figura{
 
     private double altura;
     private double base;
 
-    public Retangulo() {
+    public Retangulo(){
         this("", 0, 0);
     }
 
-    public Retangulo(String cor, double base, double altura) {
+    public Retangulo(String cor, double base, double altura){
         super(cor);
         setBase(base);
         setAltura(altura);
     }
 
-    public void setAltura(double altura) {
+    public void setAltura(double altura){
         this.altura = altura;
     }
 
-    public void setBase(double base) {
+    public void setBase(double base){
         this.base = base;
     }
 
-    public double getAltura() {
+    public double getAltura(){
         return altura;
     }
 
-    public double getBase() {
+    public double getBase(){
         return base;
     }
 
     @Override
-    public double getArea() {
+    public double getArea(){
         return (getBase() * getAltura());
     }
 }
