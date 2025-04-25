c1112ea513c72dfde3cb67c5aa5109a8fc7a0153
Franco <francobmaranhao@gmail.com>
2025-04-25 16:32:31 -0300

    Update Triangulo.java

--- src/main/java/figuras/Triangulo.java
+++ src/main/java/figuras/Triangulo.java
@@ -1,39 +1,39 @@
 package figuras;
 
-public class Triangulo extends Figura {
+public class Triangulo extends Figura{
 
     private double altura;
 
     private double base;
 
-    public Triangulo() {
+    public Triangulo(){
         this("", 0, 0);
     }
 
-    public Triangulo(String cor, double base, double altura) {
+    public Triangulo(String cor, double base, double altura){
         super(cor);
         setBase(base);
         setAltura(altura);
     }
 
-    public double getAltura() {
+    public double getAltura(){
         return altura;
     }
    
-    public double getBase() {
+    public double getBase(){
         return base;
     }
 
-    public void setAltura(double altura) {
+    public void setAltura(double altura){
         this.altura = altura;
     }
 
-    public void setBase(double base) {
+    public void setBase(double base){
         this.base = base;
     }
 
     @Override
-    public double getArea() {
+    public double getArea(){
         return ((getBase() * getAltura()) / 2.0);
     }
 }
